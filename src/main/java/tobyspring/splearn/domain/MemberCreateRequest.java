f0f21f7267e1aa58d6827b0cb945165f4dd6417d
jungyongjin <jyj1641@gmail.com>
2025-07-17 17:01:02 +0900

    섹션 4.도메인 모델 패턴으로 회원 도메인 개발 17.Member 도메인 코드 개선

---
+++ src/main/java/tobyspring/splearn/domain/MemberCreateRequest.java
@@ -0,0 +1,5 @@
+package tobyspring.splearn.domain;
+
+public record MemberCreateRequest(String email, String nickname, String password) {
+
+}
---
+++ src/main/java/tobyspring/splearn/domain/PasswordEncoder.java
@@ -0,0 +1,6 @@
+package tobyspring.splearn.domain;
+
+public interface PasswordEncoder {
+    String encode(String password);
+    boolean matches(String password, String passwordHash);
+}
