9a43a26508f3df76b1bfa276659042b2534eb3b4
jungyongjin <jyj1641@gmail.com>
2025-08-07 18:01:00 +0900

    섹션 8.웹 API 어댑터 개발
    42. API 테스트와 ProblemDetail 예외 핸들러 개발

--- src/main/java/tobyspring/splearn/adapter/webapi/dto/MemberRegisterResponse.java
+++ src/main/java/tobyspring/splearn/adapter/webapi/dto/MemberRegisterResponse.java
@@ -2,7 +2,7 @@ package tobyspring.splearn.adapter.webapi.dto;
 
 import tobyspring.splearn.domain.member.Member;
 
-public record MemberRegisterResponse(Long memberId, String emailAddress) {
+public record MemberRegisterResponse(Long memberId, String email) {
     public static MemberRegisterResponse of(Member member) {
         return new MemberRegisterResponse(member.getId(), member.getEmail().address());
     }
--- src/main/java/tobyspring/splearn/domain/member/DuplicateProfileException.java
+++ src/main/java/tobyspring/splearn/domain/member/DuplicateProfileException.java
@@ -2,6 +2,6 @@ package tobyspring.splearn.domain.member;
 
 public class DuplicateProfileException extends RuntimeException {
     public DuplicateProfileException(String message) {
-
+        super(message);
     }
 }
